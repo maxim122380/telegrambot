72c0532b1aa71690f44153aad54b80b7a8afa3e8
maxim122380 <korotkovmaxim99@gmail.com>
2024-01-28 10:07:07 +0300

    htrhr

---
+++ telegrambot.py
@@ -0,0 +1,28 @@
+import telebot
+import requests
+token = ""
+
+bot = telebot.TeleBot(token)
+
+def reqapi(ip):
+    url = f"https://ipinfo.io{ip}/geo"
+    r = requests.get(url).json()
+    return r
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    msg = bot.send_message(message.chat.id, f"hello, {message.from_user.username}\n Введи ip: ")
+    bot.register_next_step_handler(msg, getip)
+
+def getip(message):
+    ip = message.text
+    res = str(reqapi(ip))
+    bot.send_message(message.chat.id, res)
+
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
+
+
